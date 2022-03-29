97e98787c76188c96bb416a61106a865cf45223c
bayezid mir <warner.roman@gmail.com>
2022-03-29 08:28:31 +0600

    routing added for pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 // import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Friends from "./components/Friends/Friends";
 import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h1>Welcome to website with React Router</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/friends" element={<Friends />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h3 style={{ color: "grey" }}>Error 404</h3>
+      <h4 style={{ color: "red" }}>Page Not Found!!!</h4>
+    </div>
+  );
+};
+
+export default NotFound;
